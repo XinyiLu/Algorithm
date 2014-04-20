d4cce2d95e1db79ca14c00d5df7a1bc7f6e4e4c3
XinyiLu <xinyi_lu@brown.edu>
2014-04-19 21:52:05 -0400

    three sum closest

--- ReverseInt.cpp
+++ ReverseInt.cpp
@@ -5,6 +5,7 @@
 #include <sstream>
 #include <limits.h>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
 
@@ -164,9 +165,134 @@ string longestCommonPrefix(vector<string> &strs) {
 	return prefix;
 }
 
+vector<vector<int> > threeSum(vector<int> &num) {
+	vector<vector<int>> result;
+    if(num.size()<3)
+        return result;
+	sort(num.begin(),num.end()); 
+
+	return threeSumHelper(num,0,3,0);
+}
+
+
+vector<vector<int>> threeSumHelper(vector<int> &num,int start,int index,int sum){
+	vector<vector<int>> result;
+	if(index==0){
+		if(sum==0){
+			vector<int> newVector;
+			result.push_back(newVector);
+		}
+		return result;
+	}
+
+	for(int i=start;i<=num.size()-index&&num[i]<=sum;i++){
+		if(i>start&&num[i]==num[i-1]){
+			continue;
+		}
+		vector<vector<int>> subset=threeSumHelper(num,i+1,index-1,sum-num[i]);
+		for(int j=0;j<subset.size();j++){
+			vector<int> temp=subset[j];
+			temp.insert(temp.begin(),num[i]);
+			result.push_back(temp);
+		}
+	}
+	return result;
+}
+
+vector<vector<int> > fourSum(vector<int> &num, int target) {
+	vector<vector<int>> result;
+	if(num.size()<4){
+		return result;
+	}
+	sort(num.begin(),num.end());
+	for(int i=0;i<=num.size()-4;i++){
+		if(i>0&&num[i]==num[i-1]){
+			continue;
+		}
+		for(int j=i+1;j<=num.size()-3;j++){
+			if(j>i+1&&num[j]==num[j-1]){
+				continue;
+			}
+			int x=j+1,y=num.size()-1;
+			int remain=target-num[i]-num[j];
+			while(x<y){
+				while(x<y&&x>j+1&&num[x]==num[x-1]){
+					x++;
+				}
+
+				while(x<y&&y<num.size()-1&&num[y]==num[y+1]){
+					y--;
+				}
+
+				if(x>=y){
+					break;
+				}
+				int tempSum=num[x]+num[y];
+				if(tempSum==remain){
+					int tempUnit[]={num[i],num[j],num[x],num[y]};
+					vector<int> unit(tempUnit,tempUnit+sizeof(tempUnit)/sizeof(int));
+					result.push_back(unit);
+					x++;
+					y--;
+				}else if(tempSum<remain){
+					int originX=num[x];
+					while(x<y&&num[x]<=originX){
+						x++;
+					}
+					
+				}else{
+					int originY=num[y];
+					while(x<y&&num[y]>=originY){
+						y--;
+					}
+				
+				}
+			
+			}
+			
+		}
+	
+	}
+
+	return result;
+
+}
+
+void threeSumClosestHelper(vector<int> &num,int target,int sum,int start,int index,int *closest){
+        if(index==0){
+			if(abs(sum-target)<abs((*closest)-target)){
+				*closest=sum;
+			}
+			return;
+        }
+        
+		for(int i=start;i<=num.size()-index;i++){
+			if(i>start&&num[i]==num[i-1])
+				continue;
+			threeSumClosestHelper(num,target,sum+num[i],i+1,index-1,closest);
+			if(*closest==target){
+			    return;
+			}
+		}
+
+ }
+
+int threeSumClosest(vector<int> &num, int target) {
+		if(num.size()<3)
+			return 0;
+        int closest=num[0]+num[1]+num[2];
+		int *pointer=&closest;
+		threeSumClosestHelper(num,target,0,0,3,pointer);
+		return *pointer;
+}
+    
+
+
 int main(){
 	int value;
-	cout<<isMatch("ab",".*c");
+	int a[]={0,1,2};
+	vector<int> v(a,a+sizeof(a)/sizeof(int));
+	cout<<threeSumClosest(v,0);
 	cin>>value;
 	return 1;
 }
